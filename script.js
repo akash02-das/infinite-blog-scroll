5488ec5e4bc17f2ed3f2dabe9e02638420f15d22
akash <aku.das02@gmail.com>
2020-07-31 23:18:30 +0600

    filter fetched posts

--- script.js
+++ script.js
@@ -47,6 +47,23 @@ function showLoading() {
     }, 1000);
 }
 
+// Filter post by input
+function filterPosts(e) {
+    const term = e.target.value.toUpperCase();
+    const posts = document.querySelectorAll('.post');
+
+    posts.forEach(post => {
+        const title = post.querySelector('.post-title').innerText.toUpperCase();
+        const body = post.querySelector('.post-body').innerText.toUpperCase();
+
+        if (title.indexOf(term) > -1 || body.indexOf(term) > -1) {
+            post.style.display = 'flex';
+        } else {
+            post.style.display = 'none';
+        }
+    });
+}
+
 // Show initial posts
 showPosts();
 
@@ -60,4 +77,6 @@ window.addEventListener('scroll', () => {
     if (scrollTop + clientHeight >= scrollHeight - 5) {
         showLoading();
     }
-})
+});
+
+filter.addEventListener('input', filterPosts);
